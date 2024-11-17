5706a10a2642df14711d956c9d9da5fc7e5e0c22
HitanshuShahare <hitanshushahare@gmail.com>
2024-11-17 16:50:05 +0530

    Add new file

---
+++ pages/homePage.tsx
@@ -0,0 +1,9 @@
+const homePage = () => {
+    return ( 
+        <div  >
+            <p className="bg-slate-900 h-screen"> this will display homescreen</p> 
+        </div>
+     );
+}
+ 
+export default homePage;
